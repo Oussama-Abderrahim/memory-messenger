b151d96c1b37a89c32e4b5aa08927ff8087bc2cb
Oussama Abderrahim <OussamaLgt@Gmail.com>
2020-05-22 12:04:29 +0100

    Add conversation meta data (preview and time) Some Refactoring

--- src/store/conversationsStore.js
+++ src/store/conversationsStore.js
@@ -1,21 +1,21 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import DateFormatter from "@/utils/DateFormatter";
 
 Vue.use(Vuex);
 
 const state = {
   DEFAULT: {
     SENDER_NAME: "utilisateur",
-    AVATAR:
-      "https://lh3.googleusercontent.com/-mGkGQtgsLjA/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rcAQmY3uszZ1HR-gE4VfvMMJkiSNQ.CMID/s96-c/photo.jpg",
+    AVATAR: "https://www.atlantic-avitaillement.fr/wp-content/uploads/765-default-avatar.png",
     CONV: {
       title: "",
       messages: [],
-      participants: []
-    }
+      participants: [],
+    },
   },
   currentIndex: -1,
-  conversations: []
+  conversations: [],
 };
 
 const mutations = {
@@ -25,15 +25,13 @@ const mutations = {
   },
   SET_INDEX: (state, i) => {
     state.currentIndex = i;
-  }
+  },
 };
 
 const getters = {
-  currentConversation: state =>
-    state.conversations.length > 0
-      ? state.conversations[state.currentIndex]
-      : state.DEFAULT.CONV,
-  conversations: state => state.conversations
+  currentConversation: (state) =>
+    state.conversations.length > 0 ? state.conversations[state.currentIndex] : state.DEFAULT.CONV,
+  conversations: (state) => state.conversations,
 };
 
 const actions = {
@@ -42,43 +40,50 @@ const actions = {
   },
   addConversation: (store, conv) => {
     let conversation = {
-      title: conv.title
+      title: conv.title,
     };
 
     /* Load participants */
-    conversation.participants = conv.participants.map(participant => {
+    conversation.participants = conv.participants.map((participant) => {
       return {
         name: participant.name,
-        avatar: participant.avatar || state.DEFAULT.AVATAR
+        avatar: participant.avatar || state.DEFAULT.AVATAR,
       };
     });
 
-    /* Load messages */
+    conversation.preview = conv.messages[0].content;
+    conversation.time = DateFormatter.formatDate(conv.messages[0].timestamp_ms);
 
+    /* Load messages */
     conversation.messages = [];
 
     // Reverse the conversation to start from the first
     let reversedMsg = conv.messages.reverse();
 
+    let prevMsg = null;
+
     for (let message of reversedMsg) {
-      let prevMsg = conversation.messages[conversation.messages.length - 1];
+      let newMessage = {};
+
       // if same sender_name, just concat
       if (prevMsg && message.sender_name == prevMsg.sender_name) {
         prevMsg.content += "\n" + (message.content || "");
-        prevMsg.photos = prevMsg.photos
-          ? prevMsg.photos.concat(message.photos)
-          : message.photos;
-      } else {
-        conversation.messages.push({
+        prevMsg.photos = prevMsg.photos ? prevMsg.photos.concat(message.photos) : message.photos;
+      }
+      // Else add a new message
+      else {
+        newMessage = {
           content: message.content || "",
           sender_name: message.sender_name || conversation.participants[0].name,
           timestamp: message.timestamp_ms,
-          photos: message.photos
-        });
+          photos: message.photos,
+        };
+        conversation.messages.push(newMessage);
+        prevMsg = newMessage;
       }
     }
     store.commit("ADD_CONV", conversation);
-  }
+  },
 };
 
 let store = new Vuex.Store({
@@ -86,7 +91,7 @@ let store = new Vuex.Store({
   mutations: mutations,
   getters: getters,
   actions: actions,
-  strict: true
+  strict: true,
 });
 
 export default store;
