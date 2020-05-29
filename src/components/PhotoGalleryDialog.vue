d668b48ebf62094857837d9ec6c085c92c040dbf
Oussama Abderrahim <OussamaLgt@Gmail.com>
2020-05-29 01:16:34 +0100

    Add infinite scroll in photo gallery

--- src/components/PhotoGalleryDialog.vue
+++ src/components/PhotoGalleryDialog.vue
@@ -1,10 +1,10 @@
 <template>
-  <v-dialog v-model="value" width="500">
+  <v-dialog v-model="showDialog" width="600">
     <v-card class="photo-gallery">
-      <perfect-scrollbar @ps-y-reach-end="1">
+      <perfect-scrollbar @ps-y-reach-end="showMoreImages">
         <v-container fluid>
           <v-row>
-            <v-col v-for="(image, i) in images" :key="i" class="d-flex child-flex" cols="4">
+            <v-col v-for="(image, i) in shownImages" :key="i" class="d-flex child-flex" cols="4">
               <v-card flat tile class="d-flex">
                 <v-img :src="image.src" aspect-ratio="1" class="grey lighten-2 image">
                   <template v-slot:placeholder>
@@ -34,14 +34,29 @@ export default {
       default: () => ({ index: 0, src: "" })
     }
   },
-  data: () => ({}),
+  data: () => ({
+    showDialog: false,
+    shownImages: [],
+    imageCount: 0
+  }),
   watch: {
     value() {
-      this.$emit("input", this.value);
+      this.showDialog = this.value;
+      if (!this.shownImages.length) {
+        this.shownImages = this.images.slice(0, 12);
+        this.imageCount = this.shownImages.length;
+      }
+    },
+    showDialog() {
+      this.$emit("input", this.showDialog);
     }
   },
-  mounted() {
-    console.log(this.images);
+  methods: {
+    showMoreImages() {
+      if (this.imageCount >= this.images.length) return;
+      this.shownImages = this.images.slice(0, this.imageCount + 6);
+      this.imageCount = this.shownImages.length;
+    }
   }
 };
 </script>
